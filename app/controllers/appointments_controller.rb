4694d866d674f7d36804bd0c24dc75c3c03a63bd
ale_guglielmoni <alessandro.guglielmoni@technevalue.com>
2021-03-08 01:17:00 +0100

    fixes

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -82,6 +82,8 @@ class AppointmentsController < ApplicationController
         Appointment.new({
           title: "Morning",
           daysOfWeek: ['1', '2', '3', '4', '5'].to_json,
+          start: 0,
+          end: 0,
           startTime: "00:00:00",
           endTime: "08:00:00",
           backgroundColor: "#592321",
@@ -92,6 +94,8 @@ class AppointmentsController < ApplicationController
         Appointment.new({
           title: "Evening",
           daysOfWeek: ['1', '2', '3', '4', '5'].to_json,
+          start: 0,
+          end: 0,
           startTime: "18:00:00",
           endTime: "24:00:00",
           backgroundColor: "#592321",
@@ -102,8 +106,22 @@ class AppointmentsController < ApplicationController
         Appointment.new({
           title: "Lunch",
           daysOfWeek: ['1', '2', '3', '4', '5'].to_json,
+          start: 0,
+          end: 0,
           startTime: "12:00:00",
           endTime: "13:00:00",
+          backgroundColor: "#592321",
+          borderColor: "#000000",
+          fixed: true,
+          recurring: true
+        }).save
+        Appointment.new({
+          title: "Weekend",
+          daysOfWeek: ['0', '6'].to_json,
+          start: 0,
+          end: 0,
+          startTime: "00:00:00",
+          endTime: "24:00:00",
           backgroundColor: "#592321",
           borderColor: "#000000",
           fixed: true,
@@ -112,8 +130,8 @@ class AppointmentsController < ApplicationController
 
         Appointment.new({
           title: "1",
-          start: DateTime.parse("#{Date.today}T10:15:00+01:00").to_time.to_i * 1000,
-          end: DateTime.parse("#{Date.today}T10:30:00+01:00").to_time.to_i * 1000,
+          start: DateTime.parse("#{Date.today.monday}T10:15:00+01:00").to_time.to_i * 1000,
+          end: DateTime.parse("#{Date.today.monday}T10:30:00+01:00").to_time.to_i * 1000,
           allDay: false,
           backgroundColor: "#808080",
           borderColor: "#000000",
@@ -122,8 +140,8 @@ class AppointmentsController < ApplicationController
         }).save
         Appointment.new({
           title: "2",
-          start: DateTime.parse("#{Date.today + 1.days}T11:00:00+01:00").to_time.to_i * 1000,
-          end: DateTime.parse("#{Date.today + 1.days}T11:30:00+01:00").to_time.to_i * 1000,
+          start: DateTime.parse("#{Date.today.monday + 1.days}T11:00:00+01:00").to_time.to_i * 1000,
+          end: DateTime.parse("#{Date.today.monday + 1.days}T11:30:00+01:00").to_time.to_i * 1000,
           allDay: false,
           backgroundColor: "#808080",
           borderColor: "#000000",
@@ -132,8 +150,8 @@ class AppointmentsController < ApplicationController
         }).save
         Appointment.new({
           title: "3",
-          start: DateTime.parse("#{Date.today + 1.days}T15:30:00+01:00").to_time.to_i * 1000,
-          end: DateTime.parse("#{Date.today + 1.days}T16:30:00+01:00").to_time.to_i * 1000,
+          start: DateTime.parse("#{Date.today.monday + 1.days}T15:30:00+01:00").to_time.to_i * 1000,
+          end: DateTime.parse("#{Date.today.monday + 1.days}T16:30:00+01:00").to_time.to_i * 1000,
           allDay: false,
           backgroundColor: "#808080",
           borderColor: "#000000",
@@ -142,8 +160,8 @@ class AppointmentsController < ApplicationController
         }).save
         Appointment.new({
           title: "4",
-          start: DateTime.parse("#{Date.today + 2.days}T10:00:00+01:00").to_time.to_i * 1000,
-          end: DateTime.parse("#{Date.today + 2.days}T10:30:00+01:00").to_time.to_i * 1000,
+          start: DateTime.parse("#{Date.today.monday + 2.days}T10:00:00+01:00").to_time.to_i * 1000,
+          end: DateTime.parse("#{Date.today.monday + 2.days}T10:30:00+01:00").to_time.to_i * 1000,
           allDay: false,
           backgroundColor: "#808080",
           borderColor: "#000000",
@@ -152,8 +170,8 @@ class AppointmentsController < ApplicationController
         }).save
         Appointment.new({
           title: "5",
-          start: DateTime.parse("#{Date.today + 2.days}T11:00:00+01:00").to_time.to_i * 1000,
-          end: DateTime.parse("#{Date.today + 2.days}T12:30:00+01:00").to_time.to_i * 1000,
+          start: DateTime.parse("#{Date.today.monday + 2.days}T11:00:00+01:00").to_time.to_i * 1000,
+          end: DateTime.parse("#{Date.today.monday + 2.days}T12:30:00+01:00").to_time.to_i * 1000,
           allDay: false,
           backgroundColor: "#808080",
           borderColor: "#000000",
@@ -162,8 +180,8 @@ class AppointmentsController < ApplicationController
         }).save
         Appointment.new({
           title: "6",
-          start: DateTime.parse("#{Date.today + 2.days}T17:30:00+01:00").to_time.to_i * 1000,
-          end: DateTime.parse("#{Date.today + 2.days}T18:00:00+01:00").to_time.to_i * 1000,
+          start: DateTime.parse("#{Date.today.monday + 2.days}T17:30:00+01:00").to_time.to_i * 1000,
+          end: DateTime.parse("#{Date.today.monday + 2.days}T18:00:00+01:00").to_time.to_i * 1000,
           allDay: false,
           backgroundColor: "#808080",
           borderColor: "#000000",
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,7 +6,9 @@ class Appointment < ApplicationRecord
     date = DateTime.strptime("#{self.start/1000}",'%s').to_date
 
     Appointment.where("id <> ?", (self.id || 0)).each do |app|
-      throw :abort if (self.date_start() < app.date_end(date)) and (app.date_start(date) < self.date_end())
+      if (self.date_start() < app.date_end(date)) and (app.date_start(date) < self.date_end())
+        throw :abort if app.recurring and app.days_array.include? "#{date.wday}" or (not app.recurring)
+      end
     end
   end
 
@@ -14,9 +16,13 @@ class Appointment < ApplicationRecord
     (not recurring) and (not fixed) # I use "recurring" and "fixed" just because there are no users. Otherwise could be better.
   end
 
+  def days_array
+    JSON.parse(daysOfWeek || "[]")
+  end
+
   def date_start(date = nil)
     if recurring
-      DateTime.parse("#{date}T#{startTime}+01:00").to_time.to_i * 1000
+      DateTime.parse("#{date}T#{startTime}+01:00").to_time.to_i * 1000 # can be better with dedicated gems
     else
       self.start
     end
@@ -24,9 +30,10 @@ class Appointment < ApplicationRecord
 
   def date_end(date = nil)
     if recurring
-      DateTime.parse("#{date}T#{endTime}+01:00").to_time.to_i * 1000
+      DateTime.parse("#{date}T#{endTime}+01:00").to_time.to_i * 1000 # can be better with dedicated gems
     else
       self.end
     end
   end
+
 end
